420e3a935d26d934714fa734ea17feab5dd50805
Josh Winskill <jwinskill@gmail.com>
2014-11-20 19:42:29 -0800

    Updated network call to push to Heroku server

--- WorkWherever/MapViewController.h
+++ WorkWherever/MapViewController.h
@@ -13,12 +13,14 @@
 #import "Place.h"
 #import "InfoWindow.h"
 #import "PlaceMarker.h"
+#import "NetworkController.h"
 
 #define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 
 @interface MapViewController : UIViewController <CLLocationManagerDelegate, UISearchBarDelegate, GMSMapViewDelegate>
 @property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
 @property (strong, nonatomic) NSMutableArray *places;
+@property (strong, nonatomic) CLLocation *myLocation;
 
 -(UIView *)mapView:(GMSMapView *)mapView markerInfoWindow:(GMSMarker *)marker;
 
